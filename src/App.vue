4786c50063cebad627893c57db685216410b8146
ksan41 <aquam41@gmail.com>
2020-10-25 17:38:58 +0900

    Init project

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+   Parent counter : {{ $store.state.counter }} <br>
+   <button @click="addCounter">+</button>
+   <button @click="subCounter">-</button>
+
+   <child></child>
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+
+
+export default {
+  components:{
+    child:Child
+  },
+  methods:{
+    //이벤트 추가
+    addCounter(){
+      this.$store.state.counter++;
+    },
+    subCounter(){
+      this.$store.state.counter--;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <hr>
+    Child counter : {{  $store.state.counter }} <br>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
